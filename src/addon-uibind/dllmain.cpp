9c78b4373c521537f96e442ca772a81530698769
Seri <seri14dev@aura.ovo.run>
2023-03-12 17:04:39 +0900

    単一のエフェクト ファイルの変更に対応

--- src/addon-uibind/dllmain.cpp
+++ src/addon-uibind/dllmain.cpp
@@ -9,7 +9,7 @@
 
 struct __declspec(uuid("90209d18-7b48-40be-b2ff-9d7f894a9428")) uibind_context
 {
-    std::list<std::pair<std::string, std::string>> replaces;
+    std::list<std::tuple<std::string, std::string, std::string>> replaces;
 };
 
 static void on_init(reshade::api::effect_runtime *runtime)
@@ -23,13 +23,16 @@ static void on_destroy(reshade::api::effect_runtime *runtime)
 
 static void on_reshade_present(reshade::api::effect_runtime *runtime)
 {
+    if (!runtime->has_effects_loaded())
+        return;
+
     uibind_context &ctx = runtime->get_private_data<uibind_context>();
 
-    std::list<std::pair<std::string, std::string>> replaces = ctx.replaces;
+    std::list<std::tuple<std::string, std::string, std::string>> replaces = ctx.replaces;
     ctx.replaces.clear();
 
-    for (const auto &pair : replaces)
-        runtime->set_preprocessor_definition(pair.first.c_str(), pair.second.c_str());
+    for (const auto &replace : replaces)
+        runtime->set_preprocessor_definition(std::get<0>(replace).c_str(), std::get<1>(replace).c_str(), std::get<2>(replace).c_str());
 }
 
 static bool on_reshade_set_uniform_value(reshade::api::effect_runtime *runtime, reshade::api::effect_uniform_variable variable, const void *value, size_t size)
@@ -41,11 +44,11 @@ static bool on_reshade_set_uniform_value(reshade::api::effect_runtime *runtime, 
         return false;
 
     ui_bind[length] = '\0';
-    reshade::api::format format = reshade::api::format::unknown;
-    uint32_t out_rows = 0, out_columns = 0, out_array_length = 0;
-    runtime->get_uniform_variable_type(variable, &format, &out_rows, &out_columns, &out_array_length);
+    reshade::api::format base_type = reshade::api::format::unknown;
+    uint32_t rows = 0, cols = 0, array_length = 0;
+    runtime->get_uniform_variable_type(variable, &base_type, &rows, &cols, &array_length);
 
-    if (out_array_length > 0)
+    if (array_length != 0)
         return false;
 
     union
@@ -58,43 +61,15 @@ static bool on_reshade_set_uniform_value(reshade::api::effect_runtime *runtime, 
     std::memcpy(&uniform_value, value, std::min(sizeof(uniform_value), size));
 
     std::string str; str.reserve(40);
-#if 0
-    switch (format)
+    for (size_t row = 0; row < rows; ++row)
     {
-        case reshade::api::format::r32_typeless: // reshadefx::type::t_bool
-            str += std::format("matrix<bool,%u,%u>(", out_rows, out_columns);
-            break;
-        case reshade::api::format::r16_sint: // reshadefx::type::t_min16int:
-            str += std::format("matrix<min16int,%u,%u>(", out_rows, out_columns);
-            break;
-        case reshade::api::format::r32_sint: // reshadefx::type::t_int:
-            str += std::format("matrix<int,%u,%u>(", out_rows, out_columns);
-            break;
-        case reshade::api::format::r16_uint: // reshadefx::type::t_min16uint:
-            str += std::format("matrix<min16uint,%u,%u>(", out_rows, out_columns);
-            break;
-        case reshade::api::format::r32_uint: // reshadefx::type::t_uint:
-            str += std::format("matrix<uint,%u,%u>(", out_rows, out_columns);
-            break;
-        case reshade::api::format::r16_float: // reshadefx::type::t_min16float:
-            str += std::format("matrix<min16float,%u,%u>(", out_rows, out_columns);
-            break;
-        case reshade::api::format::r32_float: // reshadefx::type::t_float:
-            str += std::format("matrix<float,%u,%u>(", out_rows, out_columns);
-            break;
-        default: // reshade::api::format::unknown
-            break;
-    }
-#endif
-    for (size_t row = 0; row < out_rows; ++row)
-    {
-        for (size_t col = 0; col < out_columns; ++col)
+        for (size_t col = 0; col < cols; ++col)
         {
-            const size_t array_index = col * 4 + row;
-            switch (format)
+            const size_t array_index = cols * row + col;
+            switch (base_type)
             {
                 case reshade::api::format::r32_typeless: // reshadefx::type::t_bool
-                    str += uniform_value.as_uint[array_index] ? "true" : "false";
+                    str += uniform_value.as_uint[array_index] ? "1" : "0";
                     break;
                 case reshade::api::format::r16_sint: // reshadefx::type::t_min16int:
                 case reshade::api::format::r32_sint: // reshadefx::type::t_int:
@@ -118,11 +93,39 @@ static bool on_reshade_set_uniform_value(reshade::api::effect_runtime *runtime, 
     if (!str.empty())
         str.resize(str.size() - 1);
 #if 0
-    str += ')';
+    switch (format)
+    {
+        case reshade::api::format::r32_typeless: // reshadefx::type::t_bool
+            str = std::format("matrix<bool,%u,%u>(%*s)", out_rows, out_columns, str.size(), str.c_str());
+            break;
+        case reshade::api::format::r16_sint: // reshadefx::type::t_min16int:
+            str = std::format("matrix<min16int,%u,%u>(%*s)", out_rows, out_columns, str.size(), str.c_str());
+            break;
+        case reshade::api::format::r32_sint: // reshadefx::type::t_int:
+            str = std::format("matrix<int,%u,%u>(%*s)", out_rows, out_columns, str.size(), str.c_str());
+            break;
+        case reshade::api::format::r16_uint: // reshadefx::type::t_min16uint:
+            str = std::format("matrix<min16uint,%u,%u>(%*s)", out_rows, out_columns, str.size(), str.c_str());
+            break;
+        case reshade::api::format::r32_uint: // reshadefx::type::t_uint:
+            str = std::format("matrix<uint,%u,%u>(%*s)", out_rows, out_columns, str.size(), str.c_str());
+            break;
+        case reshade::api::format::r16_float: // reshadefx::type::t_min16float:
+            str = std::format("matrix<min16float,%u,%u>(%*s)", out_rows, out_columns, str.size(), str.c_str());
+            break;
+        case reshade::api::format::r32_float: // reshadefx::type::t_float:
+            str = std::format("matrix<float,%u,%u>(%*s)", out_rows, out_columns, str.size(), str.c_str());
+            break;
+        default: // reshade::api::format::unknown
+            break;
+    }
 #endif
 
+    char effect_name[256] = "";
+    runtime->get_uniform_variable_effect_name(variable, effect_name);
+
     uibind_context &ctx = runtime->create_private_data<uibind_context>();
-    ctx.replaces.emplace_back(ui_bind, str.c_str());
+    ctx.replaces.emplace_back(effect_name, ui_bind, str);
 
     return false;
 }
